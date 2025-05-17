4af884f4ea1df778085670bf5aace002fc326f6d
anwer <theonemyth99@gmail.com>
2025-05-17 15:28:33 +0300

    tables are done

--- product/models.py
+++ product/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-from categories.models import *
+from categories.models import Categories  # More explicit import
 
-
-
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Increased max_digits
     stock = models.IntegerField()
-    img = models.ImageField(upload_to='product/',blank=1)
-    catid = models.ForeignKey(to = Categories, on_delete= models.CASCADE)
+    img = models.ImageField(upload_to='products/', blank=True)  # Changed to boolean
+    catid = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+    )
+
+
+    def __str__(self):
+        return self.name
