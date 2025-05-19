e593d5bca551ca1338f97479a9e0fae05c95935c
anwer <theonemyth99@gmail.com>
2025-05-19 17:50:39 +0300

    img works

--- product/models.py
+++ product/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    img = models.ImageField(upload_to='product/', blank=True, default=1)
+    img = models.ImageField(upload_to='product/', blank=True, default='product/img.png')
     stock = models.IntegerField(default=1)
     catid = models.ForeignKey(
         to=Categories,
