f439cd3322d26cdac06a2ebcf5d72d4961d1158c
anwer <theonemyth99@gmail.com>
2025-05-20 16:09:28 +0300

    templates completly done

--- categories/urls.py
+++ categories/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     # path('', listall, name='a'),
     path('', Listing.as_view(), name='category-list'),
-    path('/new', Adding.as_view(), name='category-create'),
-    path('edit/<int:pk>/', Updating.as_view(), name='category-update'),
-    path('delete/<int:pk>/', Deleting.as_view(), name='category-delete'),
+    path('new', Adding.as_view(), name='category-create'),
+    path('edit/<int:pk>', Updating.as_view(), name='category-update'),
+    path('delete/<int:pk>', Deleting.as_view(), name='category-delete'),
 ]
