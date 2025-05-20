055925ec23810a45872cb2e0aac03783e3ef6cda
anwer <theonemyth99@gmail.com>
2025-05-20 15:36:31 +0300

    categories urls managed2

--- categories/urls.py
+++ categories/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', listall, name='a'),
-    path('categories/', Listing.as_view(), name='category-list'),
-    path('categories/new/', Adding.as_view(), name='category-create'),
-    path('categories/<int:pk>/edit/', Updating.as_view(), name='category-update'),
-    path('categories/<int:pk>/delete/', Deleting.as_view(), name='category-delete'),
+    # path('', listall, name='a'),
+    path('', Listing.as_view(), name='category-list'),
+    path('/new', Adding.as_view(), name='category-create'),
+    path('edit/<int:pk>/', Updating.as_view(), name='category-update'),
+    path('delete/<int:pk>/', Deleting.as_view(), name='category-delete'),
 ]
