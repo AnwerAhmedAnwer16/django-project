cfac30a399a7b355a9a954bc77ff9cddad1c6b8a
anwer <theonemyth99@gmail.com>
2025-05-28 22:46:40 +0300

    lab4 modified

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import User
 
-# Create your models here.
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Links to User
+class CustomUser(AbstractUser):
     phone = models.CharField(max_length=20, blank=True)
     address = models.TextField(blank=True)
-    registration_date = models.DateTimeField(auto_now_add=True)  # Auto-save on creation
+    registration_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username}'s Profile"
+        return self.username
