6a4c7109af96b0cfa6557eacd8cf398521aa6563
anwer <theonemyth99@gmail.com>
2025-05-28 06:46:32 +0300

    urls added

--- store/urls.py
+++ store/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('categories/', include('categories.urls')),
-    path('product/', include('product.urls')),
+    path('product/', include('product.urls')),path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
