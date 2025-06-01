f37b6a11e80d0f6c4d664401a30ba6e97ece93c8
anwer <theonemyth99@gmail.com>
2025-06-01 21:59:11 +0300

    importing modified

--- product/views.py
+++ product/views.py
@@ -7,10 +7,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from .models import Product
-from .serializers import ProductSerializer
 from rest_framework.generics import UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.response import Response
 
