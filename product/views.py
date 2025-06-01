6ef4fb6abb177eaa4ed0aadbcf2fa5f9d79a305c
anwer <theonemyth99@gmail.com>
2025-06-01 22:24:01 +0300

    view set

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template.context_processors import request
+from rest_framework import viewsets
 from rest_framework import generics
 from django.views.generic import *
 from django.urls import reverse_lazy
@@ -167,3 +168,6 @@ class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
         product.delete()
         return Response(status=204)
 
+class ProductViewSet(viewsets.ModelViewSet):
+    all_products = Product.objects.all()
+    how_to_serialize = ProductSerializer
