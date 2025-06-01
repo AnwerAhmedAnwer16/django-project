b0f1b527fd1e244e87b9c9a9665175651a3db3af
anwer <theonemyth99@gmail.com>
2025-06-01 21:54:11 +0300

    view 1 done

--- product/views.py
+++ product/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.template.context_processors import request
 from django.views.generic import *
 from django.urls import reverse_lazy
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Product
+from .serializers import ProductSerializer
 
 from product.forms import *
 from categories.models import Categories
@@ -90,3 +95,29 @@ class Deletingcbv(View):
         product = Product.objects.get(pk=pk)
         product.delete()
         return redirect('listing')
+
+
+@api_view(['GET', 'POST'])
+def product_list_create(request):
+
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response({
+            'success': True,
+            'count': len(serializer.data),
+            'products': serializer.data
+        })
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'success': True,
+                'product': serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            'success': False,
+            'errors': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
